11d93a4c1776fd3dfb03a2915db61ea37ed84779
Dobatymo <Dobatymo@users.noreply.github.com>
2020-04-28 18:25:56 +0800

    pypi deploy

--- setup.py
+++ setup.py
@@ -5,9 +5,21 @@ with open("readme.md", "r", encoding="utf-8") as fr:
 	long_description = fr.read()
 
 setup(
+	author="Dobatymo",
 	name="rbtv-api",
 	version="0.0.1",
-	py_modules=["rbtv"],
+	url="https://github.com/Dobatymo/rbtv-api",
+	description="Simple Python wrapper for the JSON Rocket Beans TV API",
 	long_description=long_description,
-	install_requires=["requests"]
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: ISC License (ISCL)",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 3",
+	],
+	py_modules=["rbtv"],
+	install_requires=["requests"],
+	use_2to3=False
 )
