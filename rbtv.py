d75e0bf1b7fb47fca22f6b5e60ee66e74c69816e
Dobatymo <Dobatymo@users.noreply.github.com>
2020-05-05 18:06:31 +0800

    added more endpoints and some simple examples when called as a script

--- rbtv.py
+++ rbtv.py
@@ -1,6 +1,7 @@
 import logging
 from urllib.parse import urlencode, urlunsplit
 from functools import lru_cache
+from datetime import datetime, timedelta
 
 import requests
 
@@ -9,6 +10,16 @@ def name_of_season(season, tpl="Season {}"):
 		return season["name"]
 	except KeyError:
 		return tpl.format(season["numeric"])
+
+def oauth_required(scope=None):
+
+	def decorator(func):
+		def inner(*args, **kwargs):
+			raise RuntimeError("Oauth required, but not implemented yet")
+
+		return inner
+
+	return decorator
 
 class API(object):
 
@@ -30,6 +41,18 @@ class API(object):
 		r.raise_for_status()
 		return r.json()
 
+	def _patch_request(self, path, body):
+		parts = (self.scheme, self.netloc, path, "", "")
+		url = urlunsplit(parts)
+
+		logging.debug("PATCH %s", url)
+		r = requests.patch(url, json=body, timeout=self.timeout)
+		r.raise_for_status()
+		res = r.json()
+
+		assert res["success"]
+		return res["data"]
+
 	def _request_paged(self, path, limit, flat=True, **params):
 
 		offset = 0
@@ -39,7 +62,7 @@ class API(object):
 			res = self._request(path, offset=offset, limit=limit, **params)
 			total = res["pagination"]["total"]
 			assert res["success"]
-	
+
 			if flat:
 				for item in res["data"]:
 					yield item
@@ -52,7 +75,84 @@ class API(object):
 		res = self._request(path, **params)
 		assert res["success"]
 		return res["data"]
-	
+
+	# Blog
+
+	def get_blog_posts(self):
+		# type: () -> dict
+
+		""" Returns all blog posts for the given pagination parameters.
+		"""
+
+		for blog in self._request_paged("/v1/blog/all", 50, True):
+			yield blog
+
+	def get_blog_posts_preview(self):
+		# type: () -> dict
+
+		""" Returns all blog posts.
+		"""
+
+		for blog in self._request_paged("/v1/blog/preview/all", 50, True):
+			yield blog
+
+	def get_blog_post(self, blogpost_id):
+		# type: (int, ) -> dict
+
+		""" Returns a single blog post.
+		"""
+
+		return self._request_single("/v1/blog/{}".format(blogpost_id))
+
+	def get_blog_post_preview(self, blogpost_id):
+		# type: (int, ) -> dict
+
+		return self._request_single("/v1/blog/preview/{}".format(blogpost_id))
+
+	# Bohne
+
+	def get_bohnen_portraits(self):
+		# type: () -> dict
+
+		""" Returns reduced information about all team members.
+		"""
+
+		return self._request_single("/v1/bohne/portrait/all")
+
+	def get_bohne(self, mgmtid):
+		# type: (int, ) -> dict
+
+		""" Returns information about a single team member.
+		"""
+
+		return self._request_single("/v1/bohne/{}".format(mgmtid))
+
+	def get_bohne_portrait(self, mgmtid):
+		# type: (int, ) -> dict
+
+		""" Returns reduced information about a given team member.
+		"""
+
+		return self._request_single("/v1/bohne/portrait/{}".format(mgmtid))
+
+	# CMS
+
+	def get_cms_routes(self):
+		# type: () -> dict
+
+		""" Returns all CMS routes (frontend paths which are connected to CMS pages).
+		"""
+
+		return self._request_single("/v1/cms/route/all")
+
+	def get_cms_page(self, cms_id):
+		# type: (int, ) -> dict
+
+		""" Returns the given CMS page.
+		"""
+
+		return self._request_single("/v1/cms/{}".format(cms_id))
+
 	# Frontend
 
 	def get_frontend_init_info(self):
@@ -111,7 +211,7 @@ class API(object):
 		for ep in self._request_paged("/v1/media/episode/preview/newest", 50, False, order=order):
 			yield ep
 
-	#@auth
+	@oauth_required()
 	def get_abobox_content_for_self(self):
 		# type: (int, str) -> Iterator[dict]
 
@@ -226,6 +326,152 @@ class API(object):
 		assert only in (None, "podcast")
 		return self._request_single("/v1/media/show/preview/mini/all", sortby=sortby, only=only)
 
+	# Event
+
+	def get_current_event(self):
+		# type: () -> dict
+
+		""" Returns Information about the current active RBTV Event.
+		"""
+
+		return self._request_single("/v1/rbtvevent/active")
+
+	def get_current_event_team(self, team_id):
+		# type: () -> dict
+
+		""" Returns RBTV Event Team Information, restricted to active Events.
+		"""
+
+		return self._request_single("/v1/rbtvevent/team/{id}".format(id=team_id))
+
+	@oauth_required("user.rbtvevent.read")
+	def get_current_event_joined_team(self, event_id):
+		# type: () -> dict
+
+		""" Gets the joined Team for the given RBTV Event
+			(which must be active in order to request these information).
+		"""
+
+		return self._request_single("/v1/rbtvevent/{slug}/team".format(slug=event_id))
+
+	@oauth_required("user.rbtvevent.manage")
+	def current_event_join_team(self, event_id, team_id):
+		# type: (int, int) -> dict
+
+		""" Joins the given Team for the given Event (the event must be active).
+		"""
+
+		return self._request_single("/v1/rbtvevent/{slug}/team/{id}/join".format(slug=event_id, id=team_id)) # POST
+
+	# Schedule
+
+	def get_schedule(self, startDay, endDay):
+		# type: (datetime, datetime) -> List[dict]
+
+		""" Returns the program schedule. Each day starts with the first schedule item of type 'live' or 'premiere'.
+			Most of the time this will be "MoinMoin" at 10:30 CEST,
+			except on weekends or when there are 'live'/'premiere' items at 0:00 CEST.
+		"""
+
+		assert endDay - startDay <= timedelta(days=14)
+		return self._request_single("/v1/schedule/normalized", startDay=startDay.timestamp(), endDay=endDay.timestamp())
+
+	# Shop
+
+	def get_products(self): # fixme: currently not working
+		# type: () -> dict
+
+		""" Returns information about all shop products.
+		"""
+
+		#return self._request_paged("/v1/simpleshop/product/all", 50)
+		return self._request_single("/v1/simpleshop/product/all")
+
+	# StreamCount
+
+	def get_viewer_count(self):
+		# type: () -> dict
+
+		""" Returns information about the current viewers.
+			Contains separate numbers for Youtube, Twitch, and combined.
+		"""
+
+		return self._request_single("/v1/streamcount")
+
+	# Subscription
+
+	@oauth_required("user.subscriptions.manage")
+	def subcribe(self, type_id, entity_id):
+		# type: (int, int) -> dict
+
+		return self._request_single("/v1/subscription/{type}/{id}".format(type=type_id, id=entity_id)) # POST
+
+	@oauth_required("user.subscriptions.manage")
+	def unsubscribe(self, type_id, entity_id):
+		# type: (int, int) -> dict
+
+		return self._request_single("/v1/subscription/{type}/{id}".format(type=type_id, id=entity_id)) # DELETE
+
+	@oauth_required("user.subscriptions.read")
+	def get_subscriptions(self):
+		# type: () -> dict
+
+		""" Returns all subscriptions for the current user.
+		"""
+
+		return self._request_single("/v1/subscription/mysubscriptions")
+
+	@oauth_required("user.subscriptions.read")
+	def get_subscription(self, type_id, entity_id):
+		# type: (int, int) -> dict
+
+		""" Returns notification settings for the given subscription.
+		"""
+
+		return self._request_single("/v1/subscription/{type}/{id}".format(type=type_id, id=entity_id)) # GET
+
+	@oauth_required("user.subscriptions.manage")
+	def modify_subscription(self, type_id, entity_id, subscribed=None, flags=None):
+		# type: () -> dict
+
+		""" Returns subscriptionResponse, requires subscriptionResponse in body.
+		"""
+
+		subscriptionResponse = {
+			"type": type_id,
+			"id": entity_id,
+			"subscribed": subscribed,
+			"flags": flags,
+		}
+
+		return self._patch_request("/v1/subscription/{type}/{id}".format(type=type_id, id=entity_id), subscriptionResponse)
+
+	@oauth_required("user.subscriptions.manage")
+	def modify_subscription_defaults(self, type_id, flags=None):
+		# type: () -> dict
+
+		""" Returns default notification flags for the given type.
+			Requires subscriptionDefaultResponse in body.
+		"""
+
+		subscriptionDefaultResponse = {
+			"type": type_id,
+			"flags": flags,
+		}
+
+		return self._patch_request("/v1/subscription/mydefault/{type}".format(type=type_id), subscriptionDefaultResponse)
+
+	# User
+
+	@oauth_required("user.info")
+	def get_user_info(self):
+
+		""" Returns information about the current user,
+			amount of Information depends on requested Scopes.
+		"""
+
+		return self._request_single("/v1/user/self")
+
 class RBTVAPI(API):
 
 	def get_season(self, show_id, season_id):
@@ -237,3 +483,61 @@ class RBTVAPI(API):
 				return season
 
 		raise KeyError("Season id not found")
+
+	@staticmethod
+	def _preprocess(name):
+		# type: (str, ) -> str
+
+		return name.replace(" ", "").lower() # make show name matching independent of whitespace and casing
+
+	def show_name_to_id(self, show_name):
+		# type: (str, ) -> int
+
+		d = {self._preprocess(show["title"]): int(show["id"]) for show in self.get_shows_mini()}
+		try:
+			return d[self._preprocess(show_name)]
+		except KeyError:
+			raise ValueError("Could not find show {}".format(show_name))
+
+	def bohne_name_to_id(self, bohne_name):
+		# type: (str, ) -> int
+
+		d = {self._preprocess(bohne["name"]): int(bohne["mgmtid"]) for bohne in self.get_bohnen_portraits()}
+		try:
+			return d[self._preprocess(bohne_name)]
+		except KeyError:
+			raise ValueError("Could not find bohne {}".format(bohne_name))
+
+if __name__ == "__main__":
+	from datetime import timezone
+	from itertools import islice
+
+	api = RBTVAPI()
+
+	start = datetime.now(timezone.utc)
+	end = datetime.now(timezone.utc) + timedelta(days=1)
+
+	print("Blog posts:")
+	for post in islice(api.get_blog_posts(), 10):
+		print("id={} {}".format(post["id"], post["title"]))
+	print("-"*20)
+
+	print("Blog post id=100:", api.get_blog_post(100)["title"])
+
+	print("Bohnen portraits:")
+	for portrait in islice(api.get_bohnen_portraits(), 10):
+		print("id={} {}".format(portrait["mgmtid"], portrait["name"], portrait["episodeCount"]))
+	print("-"*20)
+
+	print("Bohne id=33:", api.get_bohne(33)["firstname"])
+	print("Bohne portrait id=33:", api.get_bohne_portrait(33)["name"])
+
+	print("Episodes by bohne id=33:")
+	for episode in api.get_episodes_by_bohne_preview(33)["episodes"]:
+		print("id={} {}".format(episode["id"], episode["title"]))
+	print("-"*20)
+
+	print("Current event:", api.get_current_event())
+	print("First show of the day:", api.get_schedule(start, end)[0]["elements"][0]["title"])
+	print("Shop products:", api.get_products())
+	print("Total viewers:", api.get_viewer_count()["total"])
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Rocket Beans TV API
 
-Simple Python wrapper for the JSON RBTV API. See <https://github.com/rocketbeans/rbtv-apidoc>.
+Simple Python wrapper for the RBTV JSON API. See <https://github.com/rocketbeans/rbtv-apidoc>.
+Authorization is not implemented yet. That means all endpoints which require authorization (like subscription management) don't work yet.
